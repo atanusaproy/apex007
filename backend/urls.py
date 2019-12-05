b7509730d1f607b45e8d72fa2751faecd64bb399
Your Name <atanusaproy@gmail.com>
2019-12-05 21:50:14 +0530

    dashboard work done

--- backend/urls.py
+++ backend/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard.as_view(), name="dashboard.html"),
-    path('index', views.Manage.as_view(), name="manage_index.html")
+    path('', views.Dash.as_view(), name="dashboard"),
+    path('index', views.Manage.as_view(), name="index"),
 ]
